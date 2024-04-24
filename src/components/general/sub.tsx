796b9b22d3b127af7b713bf02f0f686bb8bad747
phoenix <ayomikuntemitope.araoye@gmail.com>
2024-04-24 16:23:44 +0100

    fixes on mobile

--- src/components/general/sub.tsx
+++ src/components/general/sub.tsx
@@ -18,7 +18,7 @@ const Sub = () => {
       )}
     >
       <div className="flex flex-col w-full h-full items-center justify-center space-y-6 md:space-y-9">
-        <p className="text-[24px] md:text-[32px] lg:text-[52px] leading-[52px] font-poppins w-[70%] font-bold text-center text-white-main">
+        <p className="text-[24px] md:text-[32px] lg:text-[52px]  lg:leading-[52px] leading-7 font-poppins lg:w-[70%] font-bold text-center text-white-main">
           Get our stories delivered From us to your inbox weekly.
         </p>
         <div className="flex items-center">
@@ -28,14 +28,14 @@ const Sub = () => {
             className="lg:w-[320px] w-[212px] rounded-md border-gray-200 md:py-4 py-2 px-2 md:px-4 outline-none"
           />
           <Button
-            className="ml-4 h-[35px] lg:h-[56px] w-[109px] lg:w-[166px]"
+            className="lg:ml-4 ml-2 h-[40px] lg:h-[56px] w-[109px] lg:w-[166px]"
             variant={"outline"}
           >
             Get Started
           </Button>
         </div>
 
-        <p className="text-center text-base w-[30%] text-white-main">
+        <p className="text-center text-base lg:w-[30%] text-white-main w-[80%]">
           Get a response tomorrow if you submit by 9pm today. If we received
           after 9pm will get a reponse the following day.
         </p>
--- src/components/navigations/Header.tsx
+++ src/components/navigations/Header.tsx
@@ -32,7 +32,7 @@ export const Header = () => {
       className={cn(
         " max-[500px]:py-2   px-4 sm:px-8 xl:px-16 2xl:px-24 flex w-full justify-between items-center  transition-colors duration-500",
         scrollHeight > 200
-          ? " fixed backdrop-blur-xl top-0 left-0  z-50 -translate-y-28 opacity-0 animate-slideDown bg-white-main/90 dark:bg-gray-900/90 py-2 border-b border-gray-200 shadow-md"
+          ? " fixed backdrop-blur-xl top-0 left-0  z-50 -translate-y-28 opacity-0 animate-slideDown bg-white-main/50 py-2 border-b border-gray-200 shadow-md"
           : "sm:py-6 py-4",
         {
           "bg-white/60 ": scrollHeight > 800 && scrollHeight < 4300,
