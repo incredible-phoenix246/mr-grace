fbed5efcb4fa18f506a8500db7b6bfc3fbd9dc48
phoenix <ayomikuntemitope.araoye@gmail.com>
2024-04-27 23:10:47 +0100

    created default for backend setup

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+/**
+ * Returns a string representing the ID.
+ * @function generateId - generates a unique ID
+ * @returns A unique ID when it invokes.
+ */
+export const generateId = () => {
+  return Math.random().toString(36).substring(2, 9);
+};
