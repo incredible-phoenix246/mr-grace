14110b8a6cae8c0c8c9e086e55dd345a15835420
phoenix <ayomikuntemitope.araoye@gmail.com>
2024-04-30 20:02:29 +0100

    contact

--- src/app/api/createblog/route.ts
+++ src/app/api/createblog/route.ts
@@ -1,7 +1,47 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/utils/prisma";
 
-export const POST = () => {
+interface Links {
+  platform: string;
+  url: string;
+}
+
+interface BlogContent {
+  title: string;
+  desc: string;
+  src: string;
+  others: any;
+  cart: string;
+  links: Links;
+}
+
+// model post {
+//   id        String    @id @default(auto()) @map("_id") @db.ObjectId
+//   src       String //image or video url
+//   createdAt DateTime  @default(now())
+//   updatedAt DateTime  @updatedAt
+//   title     String // post tittle
+//   desc      String // description
+//   others    String? // other relevant details
+//   comment   comment[]
+//   views     Int? // how many people viewed it
+//   draft     Boolean? // wethered is draft or not {published or pending}
+//   isvideo   Boolean?
+//   quote     String?
+//   cart      String?   @default("art") // category
+//   links     links[] // sharable link
+
+//   @@map("Posts")
+// }
+
+export async function POST(req: Request) {
   try {
-  } catch (e: any) {}
-};
+    const {} = await req.json();
+  } catch (e: any) {
+    console.log(e);
+    return NextResponse.json({
+      status: 500,
+      message: "something went wrong",
+    });
+  }
+}
